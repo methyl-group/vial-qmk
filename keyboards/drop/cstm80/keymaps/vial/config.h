7dd891f99e174772b11a1a55f3b158613abd3de8
methyl-group <davismkessler@gmail.com>
2026-03-13 01:17:53 +0000

    Add vial keymap and config

---
+++ keyboards/drop/cstm80/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {255, 139, 18, 223, 99, 68, 252, 68}
+
+/* Unlock combo: hold Escape + Enter to unlock Vial */
+#define VIAL_UNLOCK_COMBO_ROWS {0, 3}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
